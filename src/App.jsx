55b93025495591da856781e89ad712921c0060ea
pxperfectmike <pxperfectmike@gmail.com>
2023-04-04 14:40:47 -0700

    add full functionality and lean swipe animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import randomWords from 'random-words';
-import { motion, useAnimation } from 'framer-motion';
+import {
+	motion,
+	useAnimation,
+	useMotionValue,
+	useTransform,
+} from 'framer-motion';
 import './App.css';
 
 const App = () => {
@@ -10,12 +15,23 @@ const App = () => {
 	const [audioUrl, setAudioUrl] = useState('');
 	const [isLoading, setIsLoading] = useState(false);
 	const [delay, setDelay] = useState(0);
+	const [nextWord, setNextWord] = useState('');
+	const [nextDefinition, setNextDefinition] = useState(null);
+	const [nextPhonetic, setNextPhonetic] = useState('');
+	const [nextAudioUrl, setNextAudioUrl] = useState('');
+
+	const x = useMotionValue(0);
+	const rotation = useTransform(x, [-300, 0, 300], [-15, 0, 15]);
+
+	const getNewWord = () => {
+		setCurrentDefinition(null);
+		setDelay(1000);
+		setCurrentWord(randomWords(1));
+	};
 
 	const handleSwipe = () => {
 		if (delay <= 0) {
-			setCurrentDefinition(null);
-			setDelay(1000);
-			setCurrentWord(randomWords(1));
+			getNewWord();
 		}
 	};
 
@@ -25,12 +41,62 @@ const App = () => {
 			`https://api.dictionaryapi.dev/api/v2/entries/en/${currentWord}`
 		);
 		const data = await response.json();
+
+		// Check if the data is empty, if so, get a new word
+		if (!data.length) {
+			handleSwipe();
+			setIsLoading(false);
+			return;
+		}
+
 		setCurrentDefinition(data[0]);
-		setPhonetic(data[0].phonetics[0].text);
-		setAudioUrl(data[0].phonetics[0].audio);
+
+		if (data[0].phonetics[1]) {
+			setPhonetic(data[0].phonetics[1].text);
+			setAudioUrl(data[0].phonetics[1].audio);
+		} else if (data[0].phonetics[0]) {
+			setPhonetic(data[0].phonetics[0].text);
+			setAudioUrl(data[0].phonetics[0].audio);
+		} else {
+			setPhonetic('');
+			setAudioUrl('');
+		}
+
 		setIsLoading(false);
-		console.log(audioUrl);
 	};
+
+	const fetchNextDefinition = async () => {
+		const response = await fetch(
+			`https://api.dictionaryapi.dev/api/v2/entries/en/${nextWord}`
+		);
+		const data = await response.json();
+		setNextDefinition(data[0]);
+
+		if (data[0].phonetics[1]) {
+			setNextPhonetic(data[0].phonetics[1].text);
+			setNextAudioUrl(data[0].phonetics[1].audio);
+		} else if (data[0].phonetics[0]) {
+			setNextPhonetic(data[0].phonetics[0].text);
+			setNextAudioUrl(data[0].phonetics[0].audio);
+		} else {
+			setNextPhonetic('');
+			setNextAudioUrl('');
+		}
+	};
+
+	useEffect(() => {
+		if (currentWord) {
+			fetchDefinition();
+			setNextWord(randomWords(1));
+			fetchNextDefinition();
+		}
+	}, [currentWord]);
+
+	useEffect(() => {
+		if (currentDefinition === null && currentWord) {
+			handleSwipe();
+		}
+	}, [currentDefinition]);
 
 	useEffect(() => {
 		if (currentWord) {
@@ -52,7 +118,12 @@ const App = () => {
 	const handleDragEnd = (event, info) => {
 		const offScreenThreshold = 150;
 		if (Math.abs(info.offset.x) > offScreenThreshold) {
-			handleSwipe();
+			setCurrentWord(nextWord);
+			setCurrentDefinition(nextDefinition);
+			setPhonetic(nextPhonetic);
+			setAudioUrl(nextAudioUrl);
+			setNextWord(randomWords(1));
+			fetchNextDefinition();
 		} else {
 			cardControls.start({
 				x: 0,
@@ -61,11 +132,16 @@ const App = () => {
 		}
 	};
 
+	const borderColor = delay <= 0 ? '#629d50' : 'red';
+
 	const cardStyle = {
 		position: 'relative',
 		backgroundColor: 'white',
 		padding: '1rem',
 		margin: '1rem',
+		border: `4px solid ${borderColor}`,
+		borderRadius: '6px',
+		maxHeight: '90vh',
 	};
 
 	const dragConstraints = {
@@ -75,16 +151,48 @@ const App = () => {
 		left: 0,
 	};
 
+	const transformMeanings = (meanings) => {
+		const uniqueMeanings = {};
+		let count = 0;
+
+		for (const meaning of meanings) {
+			if (count >= 2) break;
+
+			if (!uniqueMeanings[meaning.partOfSpeech]) {
+				uniqueMeanings[meaning.partOfSpeech] = meaning.definitions.slice(0, 2);
+				count += 1;
+			}
+		}
+
+		return Object.entries(uniqueMeanings).map(
+			([partOfSpeech, definitions]) => ({
+				partOfSpeech,
+				definitions,
+			})
+		);
+	};
+
+	const DefineList = currentDefinition
+		? transformMeanings(currentDefinition.meanings).map((meaning) => (
+				<div key={meaning.partOfSpeech}>
+					<h2>{meaning.partOfSpeech}</h2>
+					{meaning.definitions.map((definition, index) => (
+						<p key={index}>{definition.definition}</p>
+					))}
+				</div>
+		  ))
+		: null;
+
 	return (
 		<div>
 			{isLoading ? (
 				<p>Loading...</p>
 			) : (
 				<motion.div
+					style={{ x, rotate: rotation, ...cardStyle, zIndex: 2 }}
 					drag={delay <= 0 ? 'x' : false}
 					onDragEnd={handleDragEnd}
 					className='mainCard'
-					style={cardStyle}
 					dragConstraints={dragConstraints}
 					dragElastic={0.7}
 					animate={cardControls}
@@ -93,10 +201,7 @@ const App = () => {
 						<>
 							<h1 className='current-word'>{currentWord}</h1>
 							<h2>{phonetic}</h2>
-							<div>
-								<h2>{currentDefinition.meanings[0].partOfSpeech}</h2>
-								<p>{currentDefinition.meanings[0].definitions[0].definition}</p>
-							</div>
+							{DefineList}
 							{audioUrl && (
 								<div>
 									<audio src={audioUrl} id='audioPronunciation' />
@@ -114,11 +219,6 @@ const App = () => {
 						<p>No definition found</p>
 					)}
 				</motion.div>
-			)}
-			{delay > 0 && (
-				<div style={{ textAlign: 'center', marginTop: '1rem' }}>
-					Swipe will be enabled in {delay / 1000}s
-				</div>
 			)}
 		</div>
 	);
